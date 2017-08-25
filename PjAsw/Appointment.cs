f24e9a8d8c66a2968385768ba139aed7ffa94a76
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-08-25 18:46:17 +0200

    Generated 2 classes and included attributes matching the database attributes shown in the webservice solution for the PjAsw projekt.

---
+++ PjAsw/Appointment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PjAsw
+{
+    public class Appointment
+    {
+        #region ClassAttributes
+        public int AppointmentNo { get; set; }
+
+        public string AppointmentDate { get; set; }
+
+        public string PersonCPR { get; set; } 
+        #endregion
+
+    }
+}
---
+++ PjAsw/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PjAsw
+{
+    public class Person
+    {
+        #region ClassAttributes
+        public string PersonCPR { get; set; }
+
+     
+        public string PersonName { get; set; }
+        #endregion
+
+    }
+}
